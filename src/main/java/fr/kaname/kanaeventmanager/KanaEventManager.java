6a0790aa597d5cf044c06bb17ba73c01522d465f
kaname2224 <kaname2224@users.noreply.github.com>
2020-04-19 01:25:22 +0200

    First commit

---
+++ src/main/java/fr/kaname/kanaeventmanager/KanaEventManager.java
@@ -0,0 +1,11 @@
+package fr.kaname.kanaeventmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KanaEventManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Plugin Enabled !");
+    }
+}
